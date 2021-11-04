d730dac1783b39e9e853be22bd75f8f402197b09
IanWernecke <IanWernecke@protonmail.com>
2021-11-03 21:35:40 -0400

    Cleaning up a bit

--- tykes/cli.py
+++ tykes/cli.py
@@ -20,8 +20,7 @@ from loguru import logger
 
 # module imports
 from tykes import color
-from tykes.maze import Maze
-from tykes.utils import frame_rate
+from tykes.maze import run as run_maze
 
 #
 #   globals
@@ -31,76 +30,38 @@ app = typer.Typer()
 
 
 # pygame must be initialized before any fonts are declared or anything
-pygame.init()
-
-# this variable is used to hold the primary pygame window, when initialized
-# it is not initialized here because we want --help to not spawn a window
-window = None
+# pygame.init()
 
 
 @app.command(name="maze")
 def maze_cmd(width: int = 20, height: int = 20):
     """Generate a maze for the tike."""
-
-    # create the maze object and render it onto the window
-    maze = Maze(width=width, height=height)
-
-    # define the primary window size now that the maze's width and height have been defined
-    # match the maze width, maze height + information
-    window = pygame.display.set_mode((maze.px_width, maze.px_height + maze.score_surface.get_height()))
-    window.fill(color.white)
-
-    # draw the maze onto the window
-    maze.draw(window)
-    pygame.display.flip()
-
-    # define the movement keys
-    movement_keys = {pygame.K_UP: (0, -1), pygame.K_DOWN: (0, 1), pygame.K_LEFT: (-1, 0), pygame.K_RIGHT: (1, 0)}
-
-    # enter the event loop
-    # 4643 frames drawn prior to frame rate implementation
-    for _ in frame_rate(frames_per_second=60):
-
-        # for each of the events in the queue, obtain it
-        for event in pygame.event.get():
-
-            # if the window 'close' event occurs
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            # key has been pressed
-            if event.type == pygame.KEYDOWN:
-
-                # escape key pressed
-                if event.key == pygame.K_ESCAPE:
-                    sys.exit()
-
-                # backspace, move back on the trail
-                if event.key == pygame.K_BACKSPACE:
-                    if not maze.move_back():
-                        logger.warning("Failed to move backwards.")
-
-                # if an arrow key has been pressed, try to move
-                if event.key in movement_keys:
-
-                    # try to move to an adjacent point
-                    point_mod = movement_keys[event.key]
-                    point = maze.point[0] + point_mod[0], maze.point[1] + point_mod[1]
-                    maze.move(point)
-
-        # draw the maze onto the window
-        maze.draw(window)
-        pygame.display.flip()
-
-        # exit if the game is won
-        if maze.complete:
-            time.sleep(5)
-            maze.generate()
+    sys.exit(run_maze(width=width, height=height))
 
 
 @app.command("memory")
 def memory_cmd():
     """Placeholder command for the next game planned."""
+
+
+@app.command("test")
+def test_cmd():
+    """A place to test new things before they are deployed."""
+    pygame.init()
+
+    # set the initial window size, fill, display, and wait
+    window = pygame.display.set_mode((640, 480))
+    window.fill(color.white)
+
+    pygame.display.flip()
+    time.sleep(5)
+
+    # set the updated window size, fill, display, and wait
+    window = pygame.display.set_mode((900, 600))
+    window.fill(color.white)
+
+    pygame.display.flip()
+    time.sleep(5)
 
 
 if __name__ == "__main__":
--- tykes/maze.py
+++ tykes/maze.py
@@ -1,13 +1,23 @@
 # standard imports
 import random
+import time
 from typing import Tuple
 
 # installed imports
 import pygame
+from loguru import logger
 
 # module imports
 from tykes import color
-from tykes.utils import centralize, create_text_box, create_text_box_fixed, generate
+from tykes.utils import centralize, create_text_box, create_text_box_fixed, frame_rate, generate
+
+# docs: "It is safe to call the init() function for any module more than once."
+pygame.init()
+
+
+# this variable is used to hold the primary pygame window, when initialized
+# it is not initialized here because we want --help to not spawn a window
+window = None
 
 
 class Maze:
@@ -247,3 +257,64 @@ class Maze:
     def move_rel(self, mod: Tuple[int]):
         """Move relative to the current point."""
         return self.move((self.point[0] + mod[0], self.point[1] + mod[1]))
+
+
+def run(width: int, height: int):
+
+    # create the maze object and render it onto the window
+    maze = Maze(width=width, height=height)
+
+    # define the primary window size now that the maze's width and height have been defined
+    # match the maze width, maze height + information
+    window = pygame.display.set_mode((maze.px_width, maze.px_height + maze.score_surface.get_height()))
+    window.fill(color.white)
+
+    # draw the maze onto the window
+    maze.draw(window)
+    pygame.display.set_caption("Maze")
+    pygame.display.flip()
+
+    # define the movement keys
+    movement_keys = {pygame.K_UP: (0, -1), pygame.K_DOWN: (0, 1), pygame.K_LEFT: (-1, 0), pygame.K_RIGHT: (1, 0)}
+
+    # enter the event loop
+    # 4643 frames drawn prior to frame rate implementation
+    for _ in frame_rate(frames_per_second=60):
+
+        # for each of the events in the queue, obtain it
+        for event in pygame.event.get():
+
+            # if the window 'close' event occurs
+            if event.type == pygame.QUIT:
+                return 0
+
+            # key has been pressed
+            if event.type == pygame.KEYDOWN:
+
+                # escape key pressed
+                if event.key == pygame.K_ESCAPE:
+                    return 0
+
+                # backspace, move back on the trail
+                if event.key == pygame.K_BACKSPACE:
+                    if not maze.move_back():
+                        logger.warning("Failed to move backwards.")
+
+                # if an arrow key has been pressed, try to move
+                if event.key in movement_keys:
+
+                    # try to move to an adjacent point
+                    point_mod = movement_keys[event.key]
+                    point = maze.point[0] + point_mod[0], maze.point[1] + point_mod[1]
+                    maze.move(point)
+
+        # draw the maze onto the window
+        maze.draw(window)
+        pygame.display.flip()
+
+        # exit if the game is won
+        if maze.complete:
+            time.sleep(5)
+            maze.generate()
+
+    return 0
